bb01bf4c361ec8931f8ba57996f3c9525ecdfb8f
user <muhammadfx@mail.ru>
2023-05-10 12:43:11 +0300

    company2

---
+++ suka.js
@@ -0,0 +1,12 @@
+new Swiper('.photo__items', {
+	slidesPerView: 3,
+	spaceBetween: 32,
+});
+new Swiper('.video__items', {
+	slidesPerView: 2,
+	spaceBetween: 32,
+});
+new Swiper('.header__swiper', {
+	slidesPerView: 1,
+	spaceBetween: 32,
+});
